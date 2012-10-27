02f8b2c879c74f9e2a809ed20f195626674619ff
Roly Fentanes <roly426@gmail.com>
2012-10-27 01:13:20 -0700

    updated example to reflect new api

--- examples/client.js
+++ examples/client.js
@@ -2,14 +2,15 @@ var jsonrpc = require('jsonrpc-tcp');
 
 
 var client = jsonrpc.createClient();
-client.connect(7000, function(remote) {
-  remote.call('echo', 'Hello World', function(err, result) {
-    console.log(result);
-  });
-  remote.call('math.add', 3, 2.1, function(err, result) {
-    console.log(result);
-  });
+client.connect(7000);
+
+client.call('echo', 'Hello World', function(err, result) {
+  console.log(err, result);
 });
 
-client.addListener('close', function() {
+client.call('math.add', 3, 2.1, function(err, result) {
+  console.log(err, result);
+});
+
+client.on('close', function() {
 });
